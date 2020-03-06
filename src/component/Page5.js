05f154bec0cc3e45cfbd5b0a28beb3cdd76cc878
Jim Wang <jimcwx@gmail.com>
2020-03-05 21:11:03 -0500

    fixed more

--- src/component/Page5.js
+++ src/component/Page5.js
@@ -6,10 +6,10 @@ class Page5 extends Component {
       <div className="page">
         <div className="shapes">
           <svg height="200" width="200" class="circle">
-            <circle cx="200" cy="100" r="100" stroke="limegreen" stroke-width="10" stroke-dasharray="10" />
+            <circle cx="200" cy="100" r="100" stroke="limegreen" stroke-width="10" stroke-dasharray="10" fill="grey" />
           </svg>
           <svg width="200" height="200" class="rect">
-            <rect width="200" height="200" stroke="red" stroke-width="10" stroke-dasharray="10" />
+            <rect width="200" height="200" stroke="red" stroke-width="10" stroke-dasharray="10" fill="grey" />
           </svg>
         </div>
         <button onClick={this.props.buttonClick}>Wow is there more?</button>
